7dd71a043f28fe56c43781f1da39c098f738e743
Chris Samuel <csamuel@gmail.com>
2025-03-14 16:14:58 -0500

    Disable activateSettings

--- darwin/darwin.nix
+++ darwin/darwin.nix
@@ -288,7 +288,8 @@
     # Show Library folder
     chflags nohidden ~/Library
 
+    # TODO: Disable until I can determine why this inverts natural scrolling option
     # Following line should allow us to avoid a logout/login cycle to see changes
-    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    # /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
   '';
 }
